7b807db11df4f06951ee7ee080ab4bdb8f32a1b4
Liz A <lizethariasc14@gmail.com>
2021-08-21 13:35:54 -0700

    set up PWA manifest

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
-require("bootstrap");
+require('bootstrap');
 const img1 = require("../../assets/img/food-table.jpg");
 const img2 = require("../../assets/img/grill.jpg");
-  
+
 $(document).ready(function() {
   // First image is hard coded in index.html
   const carouselSlides = [
