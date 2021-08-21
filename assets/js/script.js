ea168d8dfcdb08c12a666f2662a90ba384c7fece
Liz A <lizethariasc14@gmail.com>
2021-08-21 10:04:28 -0700

    fixed dependencies

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,8 @@
+require("bootstrap");
 const img1 = require("../../assets/img/food-table.jpg");
 const img2 = require("../../assets/img/grill.jpg");
-require("bootstrap");
-const createEl = require("./domMethods");
-
-
+  
+$(document).ready(function() {
   // First image is hard coded in index.html
   const carouselSlides = [
     {
@@ -20,8 +19,8 @@ const createEl = require("./domMethods");
       btnText: "Purchase Tickets",
       btnUrl: "tickets.html"
     },
-  ]
-
+  ];
+  
   carouselSlides.forEach((slide, i) => {
     $('.carousel-inner').append(`
   <div class="carousel-item fullscreen-carousel" style="background-image: url('${slide.img}')">
@@ -42,3 +41,4 @@ const createEl = require("./domMethods");
     </div>
   </div>`)
   })
+});
